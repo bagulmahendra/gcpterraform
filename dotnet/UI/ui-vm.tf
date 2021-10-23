b041248da9d13817e6ff815e2592f3d337ec29f4
Mahendra <bagulmk@gmail.com>
2021-10-23 07:25:03 +0000

    first commit

---
+++ dotnet/UI/ui-vm.tf
@@ -0,0 +1,35 @@
+resource "google_compute_instance_from_machine_image" "tpl" {
+  provider = google-beta
+  name     = "mvcdotnet-vm"
+  zone     = "us-central1-a"
+
+  source_machine_image = "projects/clean-circle-322216/global/machineImages/mvcdotnet-vm-image"
+
+network_interface {
+    network = "default"
+
+    access_config {
+      // Include this section to give the VM an external ip address
+    }
+  }
+
+  
+
+  labels = {
+    app = "mvcdotnet"
+  }
+}
+
+resource "google_compute_firewall" "http-server" {
+  name    = "default-allow-http-terraform"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80","3000"]
+  }
+
+  // Allow traffic from everywhere to instances with an http-server tag
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
+} 
---
+++ java/vm.tf
@@ -0,0 +1,39 @@
+resource "google_compute_instance_from_machine_image" "tpl" {
+  provider = google-beta
+  name     = "reactvm"
+  zone     = "us-central1-a"
+
+  source_machine_image = "projects/clean-circle-322216/global/machineImages/react-v1"
+
+network_interface {
+    network = "default"
+
+    access_config {
+      // Include this section to give the VM an external ip address
+    }
+  }
+
+    
+  labels = {
+    app = "react"
+  }
+}
+
+resource "google_compute_firewall" "http-server" {
+  name    = "default-allow-http-react-terraform"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80","3000"]
+  }
+
+  // Allow traffic from everywhere to instances with an http-server tag
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
+} 
+
+
+ output "ip" {
+   value = "{google_compute_instance_from_machine_image.default.network_interface.0.access_config.0.nat_id}"
+ }
